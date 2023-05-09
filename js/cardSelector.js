b9bcf51a230df0b4b29904a2244b9fa7281484c7
Zarek <lperezsanders@gmail.com>
2023-05-09 18:15:38 +0200

    nesas

--- js/cardSelector.js
+++ js/cardSelector.js
@@ -2,21 +2,21 @@ const ibai = document.querySelector('.ibai');
 const messi = document.querySelector('.messi');
 const shakira = document.querySelector('.shakira');
 
-// Add event listeners to all elements
+// Agregar evetlisteners
 ibai.addEventListener('click', () => selectElement(ibai));
 messi.addEventListener('click', () => selectElement(messi));
 shakira.addEventListener('click', () => selectElement(shakira));
 
 function selectElement(element) {
-  // Remove the 'selected' class from all elements
+  // remueve la clase selected
   ibai.classList.remove('selected');
   messi.classList.remove('selected');
   shakira.classList.remove('selected');
   
-  // Add the 'selected' class to the clicked element
+  // La agrega al item seleccionado
   element.classList.add('selected');
   
-  // Set the border color of the selected element to green
+  // Canbia el color a verde
   ibai.style.borderColor = ibai.classList.contains('selected') ? 'green' : '';
   messi.style.borderColor = messi.classList.contains('selected') ? 'green' : '';
   shakira.style.borderColor = shakira.classList.contains('selected') ? 'green' : '';
